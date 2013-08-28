5a6c8c54f9a4ba7c5a5c4660d03801346fd2fb27
zigotica <zigotica@gmail.com>
2013-08-28 18:39:54 +0200

    refactored tests into smaller chunks

--- test/embryo.test.layout.js
+++ test/embryo.test.layout.js
@@ -2,7 +2,34 @@
 /*                                                                             EMBRYO TEST SUITE: LAYOUT
 /* ----------------------------------------------------------------------------------------------------- */
 
-describe("EMBRYO LAYOUT: .show, .hide", function() {
+describe("EMBRYO LAYOUT: .css(set/get), .show, .hide", function() {
+        var q = $( 'section .maindiv p' ),
+            h = $( '#h' ),
+            main = $( 'section .maindiv' );
+            q.css('outline','1px solid red');
+            h.css('outline','2px solid orange');
+            main.css('outline','1px solid green');
+
+    it("$('#h') should be 40px height", function() {
+        expect(h.css('height')).toEqual('40px');
+        expect(h.css('height')).not.toEqual('0');
+    });
+
+    it("$('#h') should have 2px outline", function() {
+        expect(h.css('outline-width')).toEqual('2px');
+        expect(h.css('outline-width')).not.toEqual('10px');
+    });
+
+    it("third matched paragraph should have red outline", function() {
+        expect(q.eq(2).css('outline-color')).toEqual('red');
+        expect(q.eq(2).css('outline-color')).not.toEqual('orange');
+    });
+
+    it("third matched paragraph should have 1px outline", function() {
+        expect(q.eq(2).css('outline-width')).toEqual('1px');
+        expect(q.eq(2).css('outline-width')).not.toEqual('10px');
+    });
+
         var maindiv = $( '#maindiv' ),
             s = $( 'section' );
 
